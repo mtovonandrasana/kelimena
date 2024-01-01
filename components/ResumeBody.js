f717f1cb2542344af5fe9d9d1dbf6988e59ce7a3
mtovonandrasana <mtovonandrasana@projeltineaire.com>
2024-01-01 17:53:51 +0300

    skills

--- components/ResumeBody.js
+++ components/ResumeBody.js
@@ -164,6 +164,253 @@ export default function ResumeBody() {
             {/**--//resume-experience-section--*/}
           </div>
           {/**--//col-8--*/}
+          <div className="col-12 col-lg-4 ps-lg-4">
+            <section className="resume-skills-section resume-section">
+              <h3 className="resume-section-heading text-uppercase py-2 py-lg-3 py-3">
+                <i className="resume-section-heading-icon bi bi-gear me-2"></i>
+                Tech Stack
+              </h3>
+
+              <ul className="list-unstyled">
+                <li className="mb-2">
+                  <div className="resume-skill-name">JavaScript</div>
+                  <div
+                    className="progress resume-progress"
+                    role="progressbar"
+                    aria-label="Basic example"
+                    aria-valuenow="98"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    <div
+                      className="progress-bar resume-progress-bar"
+                      style={{ width: "98%" }}
+                    ></div>
+                  </div>
+                </li>
+                <li className="mb-2">
+                  <div className="resume-skill-name">ReactJS</div>
+                  <div
+                    className="progress resume-progress"
+                    role="progressbar"
+                    aria-label="Basic example"
+                    aria-valuenow="95"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    <div
+                      className="progress-bar resume-progress-bar"
+                      style={{ width: "95%" }}
+                    ></div>
+                  </div>
+                </li>
+                <li className="mb-2">
+                  <div className="resume-skill-name">Python</div>
+                  <div
+                    className="progress resume-progress"
+                    role="progressbar"
+                    aria-label="Basic example"
+                    aria-valuenow="92"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    <div
+                      className="progress-bar resume-progress-bar"
+                      style={{ width: "92%" }}
+                    ></div>
+                  </div>
+                </li>
+                <li className="mb-2">
+                  <div className="resume-skill-name">MongoDB</div>
+                  <div
+                    className="progress resume-progress"
+                    role="progressbar"
+                    aria-label="Basic example"
+                    aria-valuenow="86"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    <div
+                      className="progress-bar resume-progress-bar"
+                      style={{ width: "86%" }}
+                    ></div>
+                  </div>
+                </li>
+              </ul>
+            </section>
+            {/**--//resume-section--*/}
+
+            <hr />
+
+            <section className="resume-skills-section resume-section">
+              <h3 className="resume-section-heading text-uppercase py-2 py-lg-3 py-3">
+                <i className="resume-section-heading-icon bi bi-person-gear me-2"></i>
+                Soft Skills
+              </h3>
+              <ul className="list-inline">
+                <li className="list-inline-item">
+                  <span className="badge resume-skill-badge">Leadership</span>
+                </li>
+                <li className="list-inline-item">
+                  <span className="badge resume-skill-badge">
+                    Project Management
+                  </span>
+                </li>
+
+                <li className="list-inline-item">
+                  <span className="badge resume-skill-badge">DevOps</span>
+                </li>
+                <li className="list-inline-item">
+                  <span className="badge resume-skill-badge">
+                    Public Speaking
+                  </span>
+                </li>
+              </ul>
+            </section>
+            {/**--//resume-section--*/}
+
+            <hr />
+
+            <section className="resume-projects-section resume-section">
+              <h3 className="resume-section-heading text-uppercase py-2 py-lg-3 py-3">
+                <i className="resume-section-heading-icon bi bi-code-slash me-2"></i>
+                Projects
+              </h3>
+              <div className="item">
+                <h4 className="item-heading">
+                  <i className="item-icon bi bi-square-fill me-2"></i>
+                  <a href="#">Project lorem Ipsum</a>
+                </h4>
+                <div className="item-desc">
+                  Project desc goes here aenean ut libero sit amet ante
+                  fermentum scelerisque non a leo.
+                </div>
+              </div>
+              {/**--//item--*/}
+              <div className="item">
+                <h4 className="item-heading">
+                  <i className="item-icon bi bi-square-fill me-2"></i>
+                  <a href="#">Volutpat App</a>
+                </h4>
+                <div className="item-desc">
+                  You can link to your project Github page.
+                </div>
+              </div>
+              {/**--//item--*/}
+            </section>
+
+            <hr />
+
+            <section className="resume-educate-section resume-section">
+              <h3 className="resume-section-heading text-uppercase py-2 py-lg-3 py-3">
+                <i className="resume-section-heading-icon bi bi-book me-2"></i>
+                Education
+              </h3>
+              <ul className="list-unstyled">
+                <li className="mb-2">
+                  <div className="resume-degree font-weight-bold">
+                    MSc in Computer Science
+                  </div>
+                  <div className="resume-degree-org">ENI University</div>
+                  <div className="resume-degree-time">2016 - 2017</div>
+                </li>
+                <li>
+                  <div className="resume-degree font-weight-bold">
+                    BSc Maths and Physics
+                  </div>
+                  <div className="resume-degree-org">
+                    University of California, Berkeley
+                  </div>
+                  <div className="resume-degree-time">2012 - 2016</div>
+                </li>
+              </ul>
+            </section>
+            {/**--//resume-section--*/}
+
+            <hr />
+
+            <section className="resume-lang-section resume-section">
+              <h3 className="resume-section-heading text-uppercase py-2 py-lg-3 py-3">
+                <i className="resume-section-heading-icon bi bi-translate me-2"></i>
+                Languages
+              </h3>
+              <ul className="list-unstyled resume-lang-list">
+                <li className="mb-2">
+                  <div className="resume-lang-name">French</div>
+                  <div className="resume-level-indicator row gx-0 flex-nowrap">
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-half"></span>
+                    </div>
+                  </div>
+                  {/** --//resume-level-indicator--*/}
+                </li>
+                <li className="mb-2">
+                  <div className="resume-lang-name">English</div>
+                  <div className="resume-level-indicator row gx-0 flex-nowrap">
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-full"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item item-half"></span>
+                    </div>
+                    <div className="col">
+                      <span className="item"></span>
+                    </div>
+                  </div>
+                  {/**--//resume-level-indicator--*/}
+                </li>
+              </ul>
+            </section>
+          </div>
+          {/*//col-side */}
         </div>
         {/** row */}
       </div>
